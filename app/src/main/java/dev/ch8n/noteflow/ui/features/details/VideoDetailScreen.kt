7efc1c6506f39e50ee84ae73f71d3afc5301fe74
ch8n <chetan.garg36@gmail.com>
2025-08-22 00:20:34 +0530

    feat: add 'Open in Youtube' button and error handling
    This commit introduces an "Open in Youtube" button to the `VideoDetailScreen`.
    
    - The button displays the video ID.
    - It attempts to open the video in the YouTube app.
    - If the YouTube app is not found, a toast message "No app found" is displayed.

--- app/src/main/java/dev/ch8n/noteflow/ui/features/details/VideoDetailScreen.kt
+++ app/src/main/java/dev/ch8n/noteflow/ui/features/details/VideoDetailScreen.kt
@@ -1,20 +1,19 @@
 package dev.ch8n.noteflow.ui.features.details
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.shape.CircleShape
@@ -39,6 +38,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -58,8 +58,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import kotlin.text.ifEmpty
 
 
 @Composable
@@ -205,12 +205,28 @@ fun VideoDetailContent(
         }
 
         stickyHeader {
+            val context = LocalContext.current
+
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .background(MaterialTheme.colorScheme.surface)
                     .padding(vertical = 8.dp),
             ) {
+                OutlinedButton(onClick = {
+                    val appIntent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:${youTubeVideo.videoId}"))
+                    try {
+                        context.startActivity(appIntent)
+                    } catch (ex: ActivityNotFoundException) {
+                        runBlocking {
+                            MessageUtil.showToast("No app found")
+                        }
+                    }
+                }) {
+                    val words = youTubeVideo.transcription.wordCount()
+                    Text("Open in Youtube 📲 | ${youTubeVideo.videoId}")
+                }
+
                 OutlinedButton(onClick = {
                     isTranscriptionBottomSheetVisible = !isTranscriptionBottomSheetVisible
                 }) {
